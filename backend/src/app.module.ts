478b3b9153b54700408e5aa76674ad2ce2051378
Thanh Tri <phamthanhtri0712@gmail.com>
2026-07-13 22:46:37 +0700

    feature(backend): setup postgre in nest

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
