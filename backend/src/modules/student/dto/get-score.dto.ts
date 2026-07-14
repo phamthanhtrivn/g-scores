23f9ce671735b8bf2c4aada257f25af5df6988fc
Thanh Tri <phamthanhtri0712@gmail.com>
2026-07-14 17:11:03 +0700

    feature(get-scores): update get score dto for better ux

--- backend/src/modules/student/dto/get-score.dto.ts
+++ backend/src/modules/student/dto/get-score.dto.ts
@@ -1,6 +1,14 @@
-import { IsNumberString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsNumberString, Length } from 'class-validator';
 
 export class GetScoreDto {
-  @IsNumberString({}, { message: 'Số báo danh phải là một chuỗi số hợp lệ' })
+  @Transform(({ value }) =>
+    typeof value === 'string' ? value.padStart(8, '0') : value,
+  )
+  @IsNumberString(
+    {},
+    { message: 'Số báo danh phải là một chuỗi số hợp lệ gồm 8 chữ số' },
+  )
+  @Length(8, 8, { message: 'Số báo danh phải có đúng 8 chữ số' })
   registrationNumber: string;
 }
